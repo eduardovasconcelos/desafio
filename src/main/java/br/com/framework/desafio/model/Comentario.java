866c72c65399c50e233925cae1c9b0a442ee78c0
Eduardo de Oliveira Vasconcelos <eduardooliveiravasconcelos@gmail.com>
2020-09-04 17:05:40 -0300

    Mudança no nome do atributo

--- src/main/java/br/com/framework/desafio/model/Comentario.java
+++ src/main/java/br/com/framework/desafio/model/Comentario.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -26,4 +27,7 @@ public class Comentario {
 	
 	@OneToOne
 	private Usuario usuario;
+	
+	@ManyToOne
+	private Post post;
 }
