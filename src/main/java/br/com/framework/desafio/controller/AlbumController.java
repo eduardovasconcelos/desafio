7c8d722c0b426c314feaffbf2f7119c8227b25eb
Eduardo de Oliveira Vasconcelos <eduardooliveiravasconcelos@gmail.com>
2020-09-04 14:22:04 -0300

    Inserido CrossOrigin nas controllers.

--- src/main/java/br/com/framework/desafio/controller/AlbumController.java
+++ src/main/java/br/com/framework/desafio/controller/AlbumController.java
@@ -3,6 +3,7 @@ package br.com.framework.desafio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import br.com.framework.desafio.model.Album;
 import br.com.framework.desafio.service.AlbumService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class AlbumController {
 
 	@Autowired
--- src/main/java/br/com/framework/desafio/controller/PostController.java
+++ src/main/java/br/com/framework/desafio/controller/PostController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import br.com.framework.desafio.model.Post;
 import br.com.framework.desafio.service.PostService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PostController {
 	
 	@Autowired
